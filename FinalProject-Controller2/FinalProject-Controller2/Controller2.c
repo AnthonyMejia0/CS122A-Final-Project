02c93b1881bd4c087aaa919bbac7b8881a7db67e
AnthonyMejia0 <mejia.anthony96@gmail.com>
2019-12-04 19:09:06 -0800

    Update Controller2.c

--- FinalProject-Controller2/FinalProject-Controller2/Controller2.c
+++ FinalProject-Controller2/FinalProject-Controller2/Controller2.c
@@ -10,23 +10,10 @@
 void initPCInt() {
 	SREG = 0x01; // Enable global interrupts
 	PCICR = 0x01; // Enable Pin Change Interrupt 0 (Pins 7...0)
-	PCMSK0 = 0x01; // Enable Pin Change Interrupt on PCINT0
-	PCMSK1 = 0x01;
+	PCMSK0 = (1 << PA0) | (1 << PA1); // Enable Pin Change Interrupt on PCINT0 & PCINT1
 }
 
 ISR(PCINT0_vect){
-	if (~PINA&0x01){
-		PORTB ^= 0x01;
-		if (currentPosition == 175){
-			setAngle(300);
-		}
-		else {
-			setAngle(175);
-		}
-	}
-}
-
-ISR(PCINT1_vect){
 	if (currentPosition == 175){
 		setAngle(300);
 		_delay_ms(3000);
@@ -43,6 +30,7 @@ int main(void)
 	initServo();
 	TimerSet(50);
 	TimerOn();
+	setAngle(175);
 	
 	while(1)
 	{
